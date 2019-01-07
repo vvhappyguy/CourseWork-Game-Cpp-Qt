11a524d3ccc95ae39d1ec977e8708a1d0cfaf5ee
Ivan Dyakin <vvhappyguy@gmail.com>
2019-01-07 17:49:43 +0300

    NetWalk game.

--- main.cpp
+++ main.cpp
@@ -1,87 +1,218 @@
 #include <SFML/Graphics.hpp>
-#include "Player.hpp"
-#include "Enviroment.hpp"
-#include "Box.hpp"
-#include <iostream>
-
-enum {stay,walk,jump,move} STATE;
+#include <time.h>
 using namespace sf;
+
+const int N = 6;
+int ts = 54; //tile size
+Vector2f offset(65, 55);
+
+Vector2i Up(0, -1);
+Vector2i Down(0, 1);
+Vector2i Right(1, 0);
+Vector2i Left(-1, 0);
+
+Vector2i DIR[4] = {Up, Right, Down, Left};
+
+struct pipe
+{
+  std::vector<Vector2i> dirs;
+  int orientation;
+  float angle;
+  bool on;
+
+  pipe() { angle = 0; }
+
+  void rotate()
+  {
+    for (int i = 0; i < dirs.size(); i++)
+      if (dirs[i] == Up)
+        dirs[i] = Right;
+      else if (dirs[i] == Right)
+        dirs[i] = Down;
+      else if (dirs[i] == Down)
+        dirs[i] = Left;
+      else if (dirs[i] == Left)
+        dirs[i] = Up;
+  }
+
+  bool isConnect(Vector2i dir)
+  {
+    for (auto d : dirs)
+      if (d == dir)
+        return true;
+    return false;
+  }
+};
+
+pipe grid[N][N];
+pipe &cell(Vector2i v) { return grid[v.x][v.y]; }
+bool isOut(Vector2i v) { return !IntRect(0, 0, N, N).contains(v); }
+
+void generatePuzzle()
+{
+  std::vector<Vector2i> nodes;
+  nodes.push_back(Vector2i(rand() % N, rand() % N));
+
+  while (!nodes.empty())
+  {
+    int n = rand() % nodes.size();
+    Vector2i v = nodes[n];
+    Vector2i d = DIR[rand() % 4];
+
+    if (cell(v).dirs.size() == 3)
+    {
+      nodes.erase(nodes.begin() + n);
+      continue;
+    }
+    if (cell(v).dirs.size() == 2)
+      if (rand() % 50)
+        continue;
+
+    bool complete = 1;
+    for (auto D : DIR)
+      if (!isOut(v + D) && cell(v + D).dirs.empty())
+        complete = 0;
+    if (complete)
+    {
+      nodes.erase(nodes.begin() + n);
+      continue;
+    }
+
+    if (isOut(v + d))
+      continue;
+    if (!cell(v + d).dirs.empty())
+      continue;
+    cell(v).dirs.push_back(d);
+    cell(v + d).dirs.push_back(-d);
+    nodes.push_back(v + d);
+  }
+}
+
+void drop(Vector2i v)
+{
+  if (cell(v).on)
+    return;
+  cell(v).on = true;
+
+  for (auto d : DIR)
+    if (!isOut(v + d))
+      if (cell(v).isConnect(d) && cell(v + d).isConnect(-d))
+        drop(v + d);
+}
 
 int main()
 {
-    RenderWindow window(VideoMode(640, 480), "Stack Attack");
+  srand(time(0));
 
-	View view( FloatRect(0, 0, 640, 480) );
+  RenderWindow app(VideoMode(390, 390), "The Pipe Puzzle!");
 
-    // Background
-    Texture bg;
-	bg.loadFromFile("assets/tmp_bg.png");
-    Sprite background(bg);
-    background.setOrigin(bg.getSize().x/2,bg.getSize().y/2);
-    background.setPosition(view.getCenter());
+  Texture t1, t2, t3, t4;
+  t1.loadFromFile("images/background.png");
+  t2.loadFromFile("images/comp.png");
+  t3.loadFromFile("images/server.png");
+  t4.loadFromFile("images/pipes.png");
+  t4.setSmooth(true);
 
-    // Enviroment
-    Enviroment env;
+  Sprite sBackground(t1), sComp(t2), sServer(t3), sPipe(t4);
+  sPipe.setOrigin(27, 27);
+  sComp.setOrigin(18, 18);
+  sServer.setOrigin(20, 20);
 
-    // Player
-    Player player(&env);
+  generatePuzzle();
 
-  
-
-    // Box
-    std::vector<Box*> boxes;
-
-    Clock clock;
-    Clock clock_box;
-
-    while(window.isOpen())
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
     {
-        float time = clock.getElapsedTime().asMicroseconds();
-        if((int)clock_box.getElapsedTime().asSeconds() == 1 && boxes.size() < 30)
-        {
-            Box* box = new Box(&env); 
-            boxes.push_back(box);
-            clock_box.restart();
-        }
-            
-            
-		clock.restart();
+      pipe &p = grid[j][i];
 
-		time = time/500;  
+      for (int n = 4; n > 0; n--) //find orientation//
+      {
+        std::string s = "";
+        for (auto d : DIR)
+          s += p.isConnect(d) ? '1' : '0';
+        if (s == "0011" || s == "0111" || s == "0101" || s == "0010")
+          p.orientation = n;
+        p.rotate();
+      }
 
-		if (time > 40) time = 40;
-
-        Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
-				window.close();
-		}
-
-        // UPDATE block
-        player.update(time);
-        for (Box* box : boxes) 
-            box->update(time);
-
-        for (Box* box : boxes)
-            if(player.getRect().intersects(box->getRect())){
-                if (player._dy>0){ player._y = box->getRect().top -  64;  player._dy=0; player.setState();}
-				if (player._dy<0){ player._y = box->getRect().top + 32 ;   player._dy=0;}
-                if (player._dx>0)	{ player._x =  box->getRect().left -  32; }
-				if (player._dx<0)	{ player._x =  box->getRect().left + box->getRect().width ;}
-            }
-
-        window.setView(view);
-        window.draw(background);
-        for (Box* box : boxes)
-        { 
-            box->draw(window);
-        }
-        player.draw(window);
-        window.display();
+      for (int n = 0; n < rand() % 4; n++) //shuffle//
+      {
+        grid[j][i].orientation++;
+        grid[j][i].rotate();
+      }
     }
 
-    for(Box* box: boxes)
-        delete box;
-    return 0;
+  Vector2i servPos;
+  while (cell(servPos).dirs.size() == 1)
+  {
+    servPos = Vector2i(rand() % N, rand() % N);
+  }
+  sServer.setPosition(Vector2f(servPos * ts));
+  sServer.move(offset);
+
+  while (app.isOpen())
+  {
+    Event e;
+    while (app.pollEvent(e))
+    {
+      if (e.type == Event::Closed)
+        app.close();
+
+      if (e.type == Event::MouseButtonPressed)
+        if (e.key.code == Mouse::Left)
+        {
+          Vector2i pos = Mouse::getPosition(app) + Vector2i(ts / 2, ts / 2) - Vector2i(offset);
+          pos /= ts;
+          if (isOut(pos))
+            continue;
+          cell(pos).orientation++;
+          cell(pos).rotate();
+
+          for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+              grid[j][i].on = 0;
+
+          drop(servPos);
+        }
+    }
+
+    app.clear();
+    app.draw(sBackground);
+
+    for (int i = 0; i < N; i++)
+      for (int j = 0; j < N; j++)
+      {
+        pipe &p = grid[j][i];
+
+        int kind = p.dirs.size();
+        if (kind == 2 && p.dirs[0] == -p.dirs[1])
+          kind = 0;
+
+        p.angle += 5;
+        if (p.angle > p.orientation * 90)
+          p.angle = p.orientation * 90;
+
+        sPipe.setTextureRect(IntRect(ts * kind, 0, ts, ts));
+        sPipe.setRotation(p.angle);
+        sPipe.setPosition(j * ts, i * ts);
+        sPipe.move(offset);
+        app.draw(sPipe);
+
+        if (kind == 1)
+        {
+          if (p.on)
+            sComp.setTextureRect(IntRect(53, 0, 36, 36));
+          else
+            sComp.setTextureRect(IntRect(0, 0, 36, 36));
+          sComp.setPosition(j * ts, i * ts);
+          sComp.move(offset);
+          app.draw(sComp);
+        }
+      }
+
+    app.draw(sServer);
+    app.display();
+  }
+
+  return 0;
 }
